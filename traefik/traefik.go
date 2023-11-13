a06c78258f32f3a20cd3b5a6c4c90bd5930d3aa7
allan <allan.nava@ies-italia.it>
2023-11-13 11:41:49 +0100

    added traefik stuff

---
+++ traefik/traefik.go
@@ -0,0 +1,50 @@
+package traefik
+
+import "github.com/go-resty/resty/v2"
+
+type traefikSdk struct {
+	Url        string
+	restClient *resty.Client
+	debug      bool
+}
+
+type ITraefikClient interface {
+	//
+	HealthCheck() error
+	IsDebug() bool
+	//
+}
+
+func (o *traefikSdk) HealthCheck() error {
+	_, err := o.restyGet(o.Url, nil)
+	if err != nil {
+		return nil
+	}
+	return nil
+}
+
+// Resty Methods
+
+func (o *traefikSdk) restyPost(url string, body interface{}) (*resty.Response, error) {
+	resp, err := o.restClient.R().
+		SetHeader("Accept", "application/json").
+		SetBody(body).
+		Post(url)
+
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// get request
+func (o *traefikSdk) restyGet(url string, queryParams map[string]string) (*resty.Response, error) {
+	resp, err := o.restClient.R().
+		SetQueryParams(queryParams).
+		Get(url)
+	//
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
